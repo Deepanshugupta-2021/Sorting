8772e25b99012a40e26f965e479814343e698dcd
Deepanshugupta-2021 <84181469+Deepanshugupta-2021@users.noreply.github.com>
2021-09-14 11:32:32 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,13 @@
+var arr = [83,1,45,95,45,52,11,47,0,45,67,82];
+
+document.write("Original Array</br>");
+document.write(arr);
+     
+document.write("</br>");
+
+
+arr.sort();
+ 
+document.write("</br>After Sorting in Ascending Order</br>");
+ 
+document.write(arr);
